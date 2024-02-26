8a28722c1a229e0901da5bc34d2bd1dc0445e724
Dinh Viet Duc <dinhvietduc.isme@gmail.com>
2024-02-26 10:49:18 +0700

    Update card

--- src/containers/Discover/index.jsx
+++ src/containers/Discover/index.jsx
@@ -7,6 +7,56 @@ export default function Discover() {
     useEffect(() => {
         AOS.init();
     }, []);
+    const discovers = [
+        {
+            imgUrl: "https://assets.nicepagecdn.com/d2cc3eaa/2602318/images/h.jpg",
+            title: "ersonal Training",
+            desc: "This apartment features an electric kettle, sofa, and private entrance.",
+        },
+        {
+            imgUrl:
+                "https://assets.nicepagecdn.com/d2cc3eaa/2602318/images/jjjjj.jpg",
+            title: "Fitness Classes",
+            desc: "In the room, designed by Space Copenhagen, every detail is",
+        },
+        {
+            imgUrl: "https://assets.nicepagecdn.com/d2cc3eaa/2602318/images/hhhh.jpg",
+            title: "Combine activities",
+            desc: "This apartment has a dining area, private entrance and kitchen.",
+        },
+        {
+            imgUrl: "https://assets.nicepagecdn.com/d2cc3eaa/2602318/images/vvv.jpg",
+            title: "Boxing Classes",
+            desc: "This apartment has a dining area, private entrance and kitchen.",
+        },
+        {
+            imgUrl:
+                "https://assets.nicepagecdn.com/d2cc3eaa/2602318/images/hhhh-16.jpg",
+            title: "Trx Training",
+            desc: "This apartment has a dining area, private entrance and kitchen.",
+        },
+        {
+            imgUrl: "https://assets.nicepagecdn.com/d2cc3eaa/2602318/images/ffff.jpg",
+            title: "ePilates and Yoga",
+            desc: "This apartment has a dining area, private entrance and kitchen.",
+        },
+        {
+            imgUrl:
+                "https://assets.nicepagecdn.com/d2cc3eaa/2602318/images/full-shot-people-barre-group-cla.jpg",
+            title: "Barre",
+            desc: "This apartment has a dining area, private entrance and kitchen.",
+        },
+        {
+            imgUrl: "https://assets.nicepagecdn.com/d2cc3eaa/2602318/images/9067.jpg",
+            title: "Cycling",
+            desc: "This apartment has a dining area, private entrance and kitchen.",
+        },
+        {
+            imgUrl: "https://assets.nicepagecdn.com/d2cc3eaa/2602318/images/765.jpg",
+            title: "Strength",
+            desc: "This apartment has a dining area, private entrance and kitchen.",
+        },
+    ];
     return (
         <div>
             <section
@@ -22,51 +72,16 @@ export default function Discover() {
                 <div
                     className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-center text-center w-fit md:w-full lg:w-fit m-auto"
                 >
+                    
+                     {discovers.map((discover, index) => (
                     <Card
-                        imageUrl="https://assets.nicepagecdn.com/d2cc3eaa/2602318/images/h.jpg"
-                        title="Personal Training"
-                        description="This apartment features a electric kettle, sofa and private entrance."
+                        key={index}
+                        imageUrl={discover.imgUrl}
+                        title={discover.title}
+                        description={discover.desc}
                     />
-                    <Card
-                        imageUrl="https://assets.nicepagecdn.com/d2cc3eaa/2602318/images/jjjjj.jpg"
-                        title="Fitness Classes"
-                        description="This apartment features a electric kettle, sofa and private entrance."
-                    />
-                    <Card
-                        imageUrl="https://assets.nicepagecdn.com/d2cc3eaa/2602318/images/hhhh.jpg"
-                        title="Combine activities"
-                        description="This apartment features a electric kettle, sofa and private entrance."
-                    />
-                    <Card
-                        imageUrl="https://assets.nicepagecdn.com/d2cc3eaa/2602318/images/vvv.jpg"
-                        title="Boxing Classes"
-                        description="This apartment features a electric kettle, sofa and private entrance."
-                    />
-                    <Card
-                        imageUrl="https://assets.nicepagecdn.com/d2cc3eaa/2602318/images/hhhh-16.jpg"
-                        title="Trx Training"
-                        description="This apartment features a electric kettle, sofa and private entrance."
-                    />
-                    <Card
-                        imageUrl="https://assets.nicepagecdn.com/d2cc3eaa/2602318/images/ffff.jpg"
-                        title="Pilates and Yoga"
-                        description="This apartment features a electric kettle, sofa and private entrance."
-                    />
-                    <Card
-                        imageUrl="https://assets.nicepagecdn.com/d2cc3eaa/2602318/images/full-shot-people-barre-group-cla.jpg"
-                        title="Barre"
-                        description="This apartment features a electric kettle, sofa and private entrance."
-                    />
-                    <Card
-                        imageUrl="https://assets.nicepagecdn.com/d2cc3eaa/2602318/images/9067.jpg"
-                        title="Cycling"
-                        description="This apartment features a electric kettle, sofa and private entrance."
-                    />
-                    <Card
-                        imageUrl="https://assets.nicepagecdn.com/d2cc3eaa/2602318/images/765.jpg"
-                        title="Strength"
-                        description="This apartment features a electric kettle, sofa and private entrance."
-                    />
+                    ))
+                     }
                 </div>
             </section>
         </div>
--- src/containers/Membership/index.jsx
+++ src/containers/Membership/index.jsx
@@ -3,12 +3,47 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Card from './card';
 export default function Membership() {
+  const membershipOptions = [
+    {
+      title: "Health",
+      features: [
+        "Unlimited classes",
+        "Daily Locker",
+        "Fitness Evaluation",
+        "Guest Passes",
+        "1 PT Sessionure",
+      ],
+      buttonText: " Inquire for rates"
+    },
+    {
+      title: "Health Plus",
+      features: [
+        "Health's Features +",
+        "Laundry Service",
+        "Overnight Locker",
+        "Guest Passes",
+        "3 PT Sessionure",
+      ],
+      buttonText: " Inquire for rates"
+    },
+    {
+      title: "Other",
+      features: [
+        "Corporate",
+        "Neighborhood",
+        "Student",
+        "Guest Passes",
+        "Spousal",
+      ],
+      buttonText: "learn more"
+    },
+  ];
   useEffect(() => {
     AOS.init();
   }, []);
   return (
     <div>
-      <section style={{ fontFamily: 'Montserrat' }} className="bg-gray-300 lg:py-[64px] mx-auto sm:pb-6 "   
+      <section style={{ fontFamily: 'Montserrat' }} className="bg-gray-300 lg:py-[64px] mx-auto sm:pb-6 "
         data-aos="fade-up"
         data-aos-easing="linear"
         data-aos-duration="6000">
@@ -16,39 +51,15 @@ export default function Membership() {
         <p className="text-center sm:text-[20px] leading-[39px] mb-8">All Memberships include full access to our amenities, pools, fitness classes & much more id est laborum.</p>
         <div className="mx-auto pb-2">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 text-center justify-center gap-8 lg:w-fit mx-8 lg:mx-auto">
-            <Card
-              title="Health"
-              features={[
-                "Health's Features +",
-                "Laundry Service",
-                "Overnight Locker",
-                "Guest Passes",
-                "3 PT Sessions"
-              ]}
-              buttonText="inquire for rates"
-            />
-            <Card
-              title="Health Plus"
-              features={[
-                "Unlimited classes",
-                "Daily Locker",
-                "Fitness Evaluation",
-                "Guest Passes",
-                "1 PT Sessionure"
-              ]}
-              buttonText="inquire for rates"
-            />
-            <Card
-              title="Other"
-              features={[
-                "Corporate",
-                "Neighborhood",
-                "Student",
-                "Guest Passes",
-                "Spousal"
-              ]}
-              buttonText="learn more"
-            />
+            {membershipOptions.map((membership, index) => (
+              <Card
+                title={membership.title}
+                features={membership.features}
+                buttonText={membership.buttonText}  
+                key={index}
+              />
+            ))
+            }
           </div>
         </div>
       </section>
